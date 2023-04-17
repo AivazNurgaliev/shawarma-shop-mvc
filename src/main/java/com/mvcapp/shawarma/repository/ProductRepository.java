5a8a36e56ce5d9875c19afcd265701a7771ca6b8
vitaly <dbnf11dbnf@gmail.com>
2023-04-17 23:52:52 +0300

    start work on product controller

---
+++ src/main/java/com/mvcapp/shawarma/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.mvcapp.shawarma.repository;
+
+import com.mvcapp.shawarma.model.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
+
+    List<ProductEntity> findByCategoryId(Integer categoryId);
+}
