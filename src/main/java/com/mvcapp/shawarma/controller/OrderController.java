4efc3ca817021522269eef4510b09998c7fd113f
Shieldbreaker Bilbo <shieldbr@pr-f1.kzn.21-school.ru>
2023-05-08 07:55:31 +0300

    after comfirm cart orders inserts into orders

--- src/main/java/com/mvcapp/shawarma/controller/OrderController.java
+++ src/main/java/com/mvcapp/shawarma/controller/OrderController.java
@@ -1,8 +1,11 @@
 package com.mvcapp.shawarma.controller;
 
 import com.mvcapp.shawarma.model.entity.OrderEntity;
+import com.mvcapp.shawarma.model.entity.UserEntity;
 import com.mvcapp.shawarma.service.OrderService;
+import com.mvcapp.shawarma.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,13 +16,15 @@ import java.util.List;
 @RequestMapping("/orders")
 public class OrderController {
     private final OrderService orderService;
+    private final UserService userService;
 
     @Autowired
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, UserService userService) {
         this.orderService = orderService;
+        this.userService = userService;
     }
 
-//    @PreAuthorized
+//        @PreAuthorized
     @GetMapping("/{userId}")
     public String getByClientId(@PathVariable Integer userId, Model model) {
         List<OrderEntity> userOrders = orderService.findByUserId(userId);
@@ -28,14 +33,16 @@ public class OrderController {
     }
 
     @GetMapping
-    public String findByUserId(Integer userId, Model model) {
-        //userServise.getId
+    public String findByUserId(Authentication authentication, Model model) {
+        UserEntity user = userService.findByEmail(authentication.getName());
+        int userId = user.getId();
         List<OrderEntity> userOrders = orderService.findByUserId(userId);
         model.addAttribute("userOrders", userOrders);
         return "order";
     }
+
     @DeleteMapping("/delete/{id}")
-    public String deleteById(@PathVariable Integer id){
+    public String deleteById(@PathVariable Integer id) {
         orderService.deleteById(id);
         return "deleted";//TODO: ???
     }
