37fc6c43534c1910a449d63d524c4b248fe3c0d7
AivazNurgaliev <aivaznurgaliev@gmail.com>
2023-05-14 19:44:06 +0300

    mwssage

--- src/main/java/com/mvcapp/shawarma/model/dto/UserCreateRequest.java
+++ src/main/java/com/mvcapp/shawarma/model/dto/UserCreateRequest.java
@@ -7,7 +7,7 @@ import org.intellij.lang.annotations.RegExp;
 @Data
 public class UserCreateRequest {
     //@Pattern(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])",
-    message = "email must be good")
+    //message = "email must be good")
     private String email;
     private String password;
     //@Pattern(regexp = "\\+\\d{1,2}\\s?(\\d{1,3})\\s?\\d{3}-\\d{4}", message = "Please enter valid phone number")
