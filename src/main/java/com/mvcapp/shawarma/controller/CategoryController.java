5e1a54a3d7d59ff80d2b4a501d286d7af136b58b
AivazNurgaliev <aivaznurgaliev@gmail.com>
2023-05-14 13:51:31 +0300

    ji est

--- src/main/java/com/mvcapp/shawarma/controller/CategoryController.java
+++ src/main/java/com/mvcapp/shawarma/controller/CategoryController.java
@@ -4,6 +4,7 @@ package com.mvcapp.shawarma.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// FIXME: 14.05.2023 delete, update, create - admin, GET - users, admins
 public class CategoryController {
 
 }
